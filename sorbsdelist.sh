8f500a110e0d16ab0ed8b1973cb5991065fd7db1
joelsondepaula <86619225+joelsondepaula@users.noreply.github.com>
2021-08-02 15:25:48 -0300

    Create sorbsdelist.sh

---
+++ sorbsdelist.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+RED='\033[0;31m'
+echo -e "================== Adicionar DESTINATÁRIO e SUBJECT referentes ao IP para delist ==================="
+echo -e "Destino:"
+read -p "" MAILR;
+#echo " "
+echo -e "Subject:"
+read -p "" SUB;
+
+echo "Hello Sorbs team, We already cleaned the account that was abusing on this server, now the queue is normal and everything is fine, may you guys proceed with delist? Thanks in advance!" | mail -s "$SUB" $MAILR
+
+echo "Solicitação de delist enviada com sucesso!" 
