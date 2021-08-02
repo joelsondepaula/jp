aef29e09f44572e40b1c2a2f38707d81e8f7229e
joelsondepaula <86619225+joelsondepaula@users.noreply.github.com>
2021-08-02 11:05:53 -0300

    Update justspamdelist.sh

--- justspamdelist.sh
+++ justspamdelist.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-tput setaf 1; echo -e "Não é necessário adicionar remetente"
+RED='\033[0;31m'
+echo -e "================== ${RED} Não há necessidade de escolher um remetente, a mensagem é enviada como root${NC} ==================="
 echo -e "To:"
 read -p "" MAILR;
 #echo " "
