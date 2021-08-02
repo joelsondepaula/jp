bed17b2087fbddb6efec406c0f06fa37c043abe4
joelsondepaula <86619225+joelsondepaula@users.noreply.github.com>
2021-08-02 16:51:56 -0300

    Update justspamdelist.sh

--- justspamdelist.sh
+++ justspamdelist.sh
@@ -9,4 +9,4 @@ read -p "" SUB;
 
 echo "Hello JustSpam team, We already cleaned the account that was abusing on this server, now the queue is normal and everything is fine, may you guys proceed with delist? Thanks in advance!" | mail -s "$SUB" $MAILR
 
-echo "Delist enviado com sucesso!" 
+echo "Solicitação de delist enviada com sucesso!" 
