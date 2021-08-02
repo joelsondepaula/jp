a21b48bf9e4098d27d8639e17c762aed88516e1e
joelsondepaula <86619225+joelsondepaula@users.noreply.github.com>
2021-08-02 10:54:44 -0300

    Update justspamdelist.sh

--- justspamdelist.sh
+++ justspamdelist.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo -e "Recipient:\n"
-read -p " " MAILR;
-#echo " "
-echo -e "Subject:\n"
-read -p " " SUB;
+#!/bin/bash
 
-echo -e "Subject: $SUB" | exim -r root@$HOSTNAME -v -odf "$MAILR"
+echo -e "Recipient:"
+read -p "" MAILR;
+#echo " "
+echo -e "Subject:"
+read -p "" SUB;
+
+echo "Hello JustSpam team, We already cleaned the account was abusing on this server, now the queue is too low and everything is fine, can you guys proceed with delist? Thanks in advance!" | mail -s "$SUB" $MAILR
