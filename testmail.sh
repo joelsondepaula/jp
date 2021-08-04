a45ee59c60f2a4479d359e6404f28055c93b950b
joelsondepaula <86619225+joelsondepaula@users.noreply.github.com>
2021-08-04 11:00:55 -0300

    Update testmail.sh

--- testmail.sh
+++ testmail.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "E-mail de teste com destino ao Hotmail" | mail -s "E-mail de Teste" testehostdime@hotmail.com
+echo -e "Subject: E-mail de teste" | exim -r root@$HOSTNAME -v -odf testehostdime@hotmail.com hostdimeteste@gmail.com
 
-echo "E-mail de teste com destino ao Gmail" | mail -s "E-mail de Teste" hostdimeteste@gmail.com
 
-echo "E-mails de teste enviados com sucesso para Hotmail e Gmail"
+
