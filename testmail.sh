e822a279863c5d92879246855fb26d8aa20b18ea
joelsondepaula <86619225+joelsondepaula@users.noreply.github.com>
2021-08-04 10:43:08 -0300

    Create testmail.sh

---
+++ testmail.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "E-mail de teste com destino ao Hotmail" | mail -s "E-mail de Teste" testehostdime@hotmail.com
+
+echo "E-mail de teste com destino ao Gmail" | mail -s "E-mail de Teste" hostdimeteste@gmail.com
+
+echo "E-mails de teste enviados com sucesso para Hotmail e Gmail"
