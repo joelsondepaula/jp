9722dac0a4e708b4026d17adc6661344dd45e887
joelsondepaula <86619225+joelsondepaula@users.noreply.github.com>
2022-02-01 11:41:55 -0300

    Update spamkfull.sh

--- spamkfull.sh
+++ spamkfull.sh
@@ -1,25 +1,44 @@
 #!/bin/bash
 LANG=en_US.utf8
 date=$(date -d "$mon 1 $year" "+%Y-%m-%d")
+
+
+rm -f retorno.txt
+touch retorno.txt
 archive='retorno.txt'
+trigger1=$(sed '3!d' retorno.txt | awk '{print $1}')
+trigger2=$(sed '24!d' retorno.txt | awk '{print $1}')
+trigger10=500
 ipsaida=$(hostname -i | tr -s " " | cut -f1 -d " ")
 
 
-cat /var/log/exim_mainlog | grep "$date" | grep "Sender identification" | awk '{print $8}' |cut -d'=' -f2 | sort | uniq -c |sort -rn | head -20 > retorno.txt
+echo -e "\nLogins used to send e-mail:" >> retorno.txt
+
+cat /var/log/exim_mainlog | grep "$date" | grep "Sender identification" | awk '{print $8}' |cut -d'=' -f2 | sort | uniq -c |sort -rn | head -21 >> retorno.txt
 
 sed -i '/mailnull/d' retorno.txt
 
-cat retorno.txt | head -n1 | awk '{print $1}' > score.txt
+echo -e "\nDirectories used to send e-mail:" >> retorno.txt
 
 
-if [ score.txt > 500 ]
+cat /var/log/exim_mainlog | grep "$date" | grep -hoP "(?<=cwd=)/[^ ]+" /var/log/exim_mainlog | sort | uniq -c | sort -nr | head -21 >> retorno.txt
 
-	then
+sed -i '\/var\/spool\/exim/d' retorno.txt && sed -i '\/etc\/csf/d' retorno.txt && sed -i '\/root/d' retorno.txt
 
-		 echo "$(cat $archive)" | mail -s "HostDime Brasil :: Possível envio de SPAM partindo do servidor $HOSTNAME de IP $ipsaida" hostdimecheck@hotmail.com
 
+if [ "$trigger1" -ge "$trigger10" ]; then
+
+                echo "$(cat $archive)" | mail -s "HostDime Brasil :: Possível envio de SPAM partindo do servidor $HOSTNAME de IP $ipsaida" hostdimecheck@hotmail.com
     else
 
-    	exit;
+                if [ "$trigger2" -ge "$trigger10" ]; then
 
+
+                                echo "$(cat $archive)" | mail -s "HostDime Brasil :: Possível envio de SPAM partindo do servidor $HOSTNAME de IP $ipsaida" hostdimecheck@hotmail.com
+
+                        else
+                            	exit;
+                fi
 fi
+
+rm -f retorno.txt
